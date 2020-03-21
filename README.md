4d7368aee964217c8b4feaf58de6a05676f49334
Christian Bjerre-Fernandes <chrisfernandes18@uchicago.edu>
2020-03-21 02:32:24 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Command Line MA
+Inspired by https://github.com/Bryce-Tucker/Command-Line-IMDB
+
+A python script that allows you to look up movies and shows on MyAnimeList.net from the command line.
+
+![Demo Gif](assets/images/cmd_demo.gif)
+
+### Required Python Modules
+Requires two third party modules:
+
+1. **requests** : A module for sending HTTP/1.1 requests using Python.
+
+2. **beautifulsoup4** : A module for parsing HTML and XML documents.
+
+### Using The Program
+Once the modules are installed, enter the directory which contains the scripts in the terminal. The name of the show or movie can be searched either just running the program and then inputing the name or or the name follows the program as a command line argument.
+---
+`python3 cmd_mal.py Black Clover`
+
+or
+
+`python3 cmd_mal.py`<br>
+`Enter the title of the anime movie or show you want to search for: Black Clover`
+---
+When a search title has been provided, the program will output the number of results on MyAnimeList.net for that title, and then give a numbered list containing the first 5 results. The user can then select from a result from the list by entering the corresponding number, request the next 5 results by entering m, or exit by hitting enter. The user is able to keep requesting more results until every result has been displayed.
+
+![More Results for Black Clover](assets/images/more_results.png)
+
+Once a result has been selected by the user, the program will output the title, rating, number of episodes, when the show aired, and synopsis. Then it will ask if the user would like to search again.
+
+![Output for Black Clover](assets/images/output.png)
+
+### Coming Soon
+Ability to make a new csv file if one does not exist that holds a list of anime that one has searched and wants to keep the information.
---
+++ cmd_mal.py
@@ -0,0 +1,308 @@
+import sys, re
+import bs4, requests
+
+def main(flag):
+
+    link, title = search_mal(get_user_input(flag))
+    
+    if link == None:
+        quit()
+    
+    mal_dict = mal_get_all_info(link, title)
+    print_mal_dict(mal_dict)
+    search_again()
+    return 0
+
+
+def get_user_input(flag): 
+    """
+    Returns a string containing user provided movie/tv title
+
+    Parameters
+    ----------
+    flag : int
+        flag is set to changed to 1 if we are searching again.
+        Otherwise the flag is 0.
+
+    Returns
+    -------
+    str
+        User input
+
+    """
+    if flag == 0:
+        if len(sys.argv) > 1:
+            return " ".join(sys.argv[1:])
+        else:
+            return input("Enter the title of the anime movie or show you want to search for: ")
+    else:
+        return input("Enter the title of the anime movie or show you want to search for: ")
+
+
+def get_soup(link):
+    """
+    Gets the HTML for a link. Throws error if it does not connect to website.
+
+    Parameters
+    ----------
+    link : str
+        URL to the webpage
+
+    Returns
+    -------
+    str
+        HTML of the link
+
+    """
+    page = requests.get(link)
+    page.raise_for_status()
+    soup = bs4.BeautifulSoup(page.text, 'html.parser')
+    return soup
+
+
+def search_mal(title, choice='m'):
+    """
+    Shows search results for given title and returns the link to user's selection.
+
+    Parameters
+    ----------
+    title : str
+        Title of anime movie or show
+    choice : str
+        Either the letter 'M' | 'm' or a number
+
+    Returns
+    -------
+    str
+        Link of user's selection
+    str
+        Title name of link
+    """
+    if title == "":
+        print("Did not provide a title.")
+        search_again()
+    
+    link = 'https://myanimelist.net/anime.php?q={}&show={}'
+    num = 0
+    search_soup = get_soup(link.format('+'.join(title.split()), num))
+    search_results = search_soup.findAll('a', class_='hoverinfo_trigger fw-b fl-l')
+
+    if search_results:
+        options = []
+
+        for search_field in search_results:
+            options.append(search_field)
+        
+        if options != []:
+            i = 0
+            length = len(options)
+            temp = 0
+
+            if (length > 5):
+                num = 5
+            else:
+                num = length
+            
+            while True:
+                print("")
+                print("{} results were found for {}".format(num, title))
+                print("")
+
+                if (i == num):
+                    i = temp
+                else:
+                    temp = i
+
+                for i in range(i, num):
+                    print('({}). {}'.format(i + 1, options[i].string))
+                    i += 1
+
+                choice = input('\nEnter the number corresponding with your choice, (M)ore for more results, or <Enter> to quit: ')
+                
+                try: 
+                    if (choice[0].lower() == 'm'):
+                        if num < length:
+                            num += 5
+                        else:
+                            search_results = get_soup(link.format('+'.join(title.split()), num)).findAll('a', class_='hoverinfo_trigger fw-b fl-l')
+                            for search_field in search_results:
+                                options.append(search_field.string)
+                            length = len(options)
+                    elif (choice.isdigit()):
+                        try:
+                            return options[int(choice) - 1].get('href'), options[int(choice) - 1].string
+                        except: 
+                            pass
+                    else:
+                        continue
+                except:
+                    quit()
+
+    else:
+        print("No Results Found")
+        return None, None
+
+
+def mal_get_all_info(link, title):
+    """
+    Given the HTML soup returns the score.
+
+    Parameters
+    ----------
+    mal_soup : str
+        HTML soup from beautiful soup
+    title : str
+        Name of anime
+
+    Returns
+    -------
+    str
+        Rating from the soup
+    """
+    anime_dict = dict()
+    mal_soup = get_soup(link)
+
+    anime_dict['title'] = title
+    anime_dict['rating'] = mal_get_rating(mal_soup)
+    anime_dict['episodes'] = mal_get_episodes(mal_soup)
+    anime_dict['aired'] = mal_get_aired(mal_soup)
+    anime_dict['synopsis'] = mal_get_synopsis(mal_soup)
+
+    return anime_dict
+
+
+def mal_get_rating(mal_soup):
+    """
+    Given the HTML soup returns the score.
+
+    Parameters
+    ----------
+    mal_soup : str
+        HTML soup from beautiful soup
+
+    Returns
+    -------
+    str
+        Rating from the soup
+    """
+    search_results = mal_soup.findAll('span', itemprop='ratingValue')
+    return search_results[0].string
+
+
+def mal_get_episodes(mal_soup):
+    """
+    Given the HTML soup returns the number of episodes.
+
+    Parameters
+    ----------
+    mal_soup : str
+        HTML soup from beautiful soup
+
+    Returns
+    -------
+    str
+        Number of episodes from the soup
+    """
+    search_results = mal_soup.findAll('span', id='curEps')
+    return search_results[0].string
+
+
+def mal_get_aired(mal_soup):
+    """
+    Given the HTML soup returns time period aired.
+
+    Parameters
+    ----------
+    mal_soup : str
+        HTML soup from beautiful soup
+
+    Returns
+    -------
+    str
+        Show's time period aired from the soup
+    """
+    search_results = mal_soup.findAll('div', class_='spaceit')
+    return search_results[1].contents[2].strip()
+
+
+def mal_get_synopsis(mal_soup):
+    """
+    Given the HTML soup returns the synopsis.
+
+    Parameters
+    ----------
+    mal_soup : str
+        HTML soup from beautiful soup
+
+    Returns
+    -------
+    str
+        Synopsis from the soup
+    """   
+    search_results = mal_soup.findAll('span', itemprop='description')
+    res = ""
+    for result in search_results:
+        for item in result.contents:
+            res += str(item).strip()
+    res = res.replace("<br/>", "\n")
+    return res
+
+
+def print_mal_dict(mal_dict):
+    """
+    Given the dictionary filled with information from anime page,
+    prints the information out to screen.
+
+    Parameters
+    ----------
+    mal_dict : dict
+        Dictionary filled with information from anime page
+
+    Returns
+    -------
+    None
+        Prints to screen
+    """
+    print("")
+    print("--------------------------------------------------------------------")
+    print("")
+    print(mal_dict['title'])
+    print("\033[1m" + "Rating: " + "\033[0m" + mal_dict['rating'])
+    print("\033[1m" + "Number of Episodes: " + "\033[0m" + mal_dict['episodes'])
+    print("\033[1m" + "Aired: " + "\033[0m" + mal_dict['aired'])
+    print("\033[1m" + "Synopsis: " + "\033[0m" + mal_dict['synopsis'])
+    print("")
+    print("--------------------------------------------------------------------")
+    print("")
+    return
+
+
+def search_again():
+    """
+    Gives the option to search again if wanted.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    function()
+        Either main() if we want to search again or 
+        quit() if we do not want to search again
+    """
+    while True:
+        choice = input("Would you like to search again? (Y/N): ")
+        try:
+            if choice[0].lower() == 'y':
+                return main(1)
+            elif choice[0].lower() == 'n':
+                return quit()
+            else:
+                print("Incorrect input. Please enter Y/y or N/n.")
+        except:
+            return quit()
+
+
+if __name__ == "__main__":
+    main(0)
