93e83062909e51b6d0bca9e7be6bfc0b1305d980
Christian Bjerre-Fernandes <chrisfernandes18@uchicago.edu>
2020-03-22 21:53:25 -0500

    can make a sorted csv file when appending to csv file

--- src/maltocsv.py
+++ src/maltocsv.py
@@ -1,4 +1,4 @@
-import csv, os.path
+import csv, os, operator
 from .cmd_mal import search_again
 
 def export_to_csv(mal_dict):
@@ -30,6 +30,7 @@ def export_to_csv(mal_dict):
                         writer = csv.DictWriter(csvfile, fieldnames=col_names)
                         writer.writerow(mal_dict)
                     print("CSV file was created and the title was added to the file.")
+                    sort_csv(csv_file, col_names)
                     return search_again()
                 except IOError:
                     print("I/O error")
@@ -48,3 +49,14 @@ def export_to_csv(mal_dict):
         else:
             print("Please enter Y or N.")
 
+
+def sort_csv(filename, col_names):
+    reader = csv.reader(open(filename), delimiter=",")
+    sortedlist = sorted(reader, key=operator.itemgetter(0), reverse=False)
+    sortedlist.remove(col_names)
+    sortedlist.insert(0, col_names)
+    os.remove(filename)
+    with open(filename, 'a+') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(sortedlist)
+    return
