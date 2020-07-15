0d5f7d18a6fc1ddfda37b36334fbab5681d81f7c
Christian Bjerre-Fernandes <chrisfernandes18@uchicago.edu>
2020-07-14 22:24:48 -0500

    deleted an else

--- src/mal_cmd.py
+++ src/mal_cmd.py
@@ -64,74 +64,73 @@ def search_mal(title):
             #except ValueError:
                # print("\nPlease input selection again\n")
         return None, None
-    else:
-        link = 'https://myanimelist.net/anime.php?q={}&show={}'
-        num = 0
-        search_soup = get_soup(link.format('+'.join(title.split()), num))
-        search_results = search_soup.findAll('a', class_='hoverinfo_trigger fw-b fl-l')
-        try:
-            path = __file__
-            path = path.replace("cmd_mal.py", "")
-            shutil.rmtree(path + "__pycache__")
-        except:
-            pass
-        if search_results:
-            options = []
+    link = 'https://myanimelist.net/anime.php?q={}&show={}'
+    num = 0
+    search_soup = get_soup(link.format('+'.join(title.split()), num))
+    search_results = search_soup.findAll('a', class_='hoverinfo_trigger fw-b fl-l')
+    try:
+        path = __file__
+        path = path.replace("cmd_mal.py", "")
+        shutil.rmtree(path + "__pycache__")
+    except:
+        pass
+    if search_results:
+        options = []
 
-            for search_field in search_results:
-                options.append(search_field)
+        for search_field in search_results:
+            options.append(search_field)
 
-            if options:
-                i = 0
-                length = len(options)
-                temp = 0
+        if options:
+            i = 0
+            length = len(options)
+            temp = 0
 
-                if length > 5:
-                    num = 5
+            if length > 5:
+                num = 5
+            else:
+                num = length
+
+            while True:
+                print("")
+                print("{} results were found for {}".format(num, title))
+                print("")
+
+                if i == num:
+                    i = temp
                 else:
-                    num = length
+                    temp = i
 
-                while True:
-                    print("")
-                    print("{} results were found for {}".format(num, title))
-                    print("")
+                for i in range(i, num):
+                    print('({}). {}'.format(i + 1, options[i].string))
+                    i += 1
 
-                    if i == num:
-                        i = temp
-                    else:
-                        temp = i
+                choice = input('\nEnter the number corresponding with your choice,' + \
+                                ' (M)ore for more results, or <Enter> to quit: ')
 
-                    for i in range(i, num):
-                        print('({}). {}'.format(i + 1, options[i].string))
-                        i += 1
-
-                    choice = input('\nEnter the number corresponding with your choice,' + \
-                                   ' (M)ore for more results, or <Enter> to quit: ')
-
-                    try:
-                        if choice[0].lower() == 'm':
-                            if num < length:
-                                num += 5
-                            else:
-                                search_results = get_soup(link.format('+'.join(title.split()),\
-                                     num)).findAll('a', class_='hoverinfo_trigger fw-b fl-l')
-                                for search_field in search_results:
-                                    options.append(search_field.string)
-                                length = len(options)
-                        elif choice.isdigit():
-                            try:
-                                return options[int(choice) - 1].get('href'), \
-                                       options[int(choice) - 1].string
-                            except:
-                                pass
+                try:
+                    if choice[0].lower() == 'm':
+                        if num < length:
+                            num += 5
                         else:
-                            continue
-                    except:
-                        quit()
+                            search_results = get_soup(link.format('+'.join(title.split()),\
+                                    num)).findAll('a', class_='hoverinfo_trigger fw-b fl-l')
+                            for search_field in search_results:
+                                options.append(search_field.string)
+                            length = len(options)
+                    elif choice.isdigit():
+                        try:
+                            return options[int(choice) - 1].get('href'), \
+                                    options[int(choice) - 1].string
+                        except:
+                            pass
+                    else:
+                        continue
+                except:
+                    quit()
 
-        else:
-            print("No Results Found")
-            return None, None
+    else:
+        print("No Results Found")
+        return None, None
 
 
 def print_mal_dict(mal_dict):
